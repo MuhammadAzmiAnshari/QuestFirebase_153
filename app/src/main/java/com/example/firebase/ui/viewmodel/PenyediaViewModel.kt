33309c21530f4af5bfecafd1fb21db2440ed95a5
Azmi <azmianshari123@gmail.com>
2025-01-10 04:58:20 +0700

    Innitial Commit

---
+++ app/src/main/java/com/example/firebase/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.firebase.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.firebase.MahasiswaApplications
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiKontak().container.mahasiswaRepository) }
+        //initializer { InsertViewModel(aplikasiKontak().container.mahasiswaRepository) }
+        //initializer { DetailViewModel(aplikasiKontak().container.mahasiswaRepository) }
+        //initializer { UpdateViewModel(aplikasiKontak().container.mahasiswaRepository) }
+    }
+
+    fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
+}
