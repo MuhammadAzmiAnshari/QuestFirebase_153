2d32d9eea4ee3621440a7e23d49ce6d527b4df7d
Azmi <azmianshari123@gmail.com>
2025-01-12 17:55:36 +0700

    Menambahkan val uiState,uiEvent, snackbarHostState, coroutineScope

--- app/src/main/java/com/example/firebase/ui/view/InsertView.kt
+++ app/src/main/java/com/example/firebase/ui/view/InsertView.kt
@@ -1,6 +1,9 @@
 package com.example.firebase.ui.view
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.firebase.ui.viewmodel.InsertViewModel
@@ -12,4 +15,9 @@ fun InsertMhsView(
     onNavigate: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel:InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+){
+    val uiState = viewModel.uiState
+    val uiEvent = viewModel.uiEvent
+    val snackbarHostState = remember {SnackbarHostState()}
+    val coroutineScope = rememberCoroutineScope()
+}
